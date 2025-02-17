ed96998a04dd0ed0073d813d1b72465da8b9ba34
Jason Desrosiers <jdesrosi@gmail.com>
2025-02-17 10:23:53 -0800

    Add reviver option to jsonast-parse and jref-parse

--- src/jref/jref-parse.js
+++ src/jref/jref-parse.js
@@ -6,22 +6,38 @@ import { fromJref } from "./jref-util.js";
  * @import { Plugin } from "unified"
  * @import { VFile } from "vfile"
  * @import { Options } from "vfile-message"
- * @import { JrefDocumentNode } from "./jref-ast.js"
+ * @import { JrefDocumentNode, JrefNode } from "./jref-ast.js"
+ * @import { Reviver } from "./jref-util.js"
  */
 
 
-/** @type Plugin<[], string, JrefDocumentNode> */
-export function jrefParse() {
+/**
+ * @typedef {{
+ *   reviver?: Reviver<JrefNode | undefined>;
+ * }} JrefParseOptions
+ */
+
+/** @type Plugin<[JrefParseOptions?], string, JrefDocumentNode> */
+export function jrefParse(options) {
   /** @type (document: string, file: VFile) => JrefDocumentNode */
   this.parser = function (document, file) {
     try {
       const uri = pathToFileURL(file.path).toString();
-      return {
+
+      /** @type JrefDocumentNode */
+      const jrefDocumentNode = {
         type: "jref-document",
-        children: [fromJref(document, uri)],
+        children: [],
         uri: uri,
         fragmentKind: "json-pointer"
       };
+
+      const jrefNode = fromJref(document, uri, options?.reviver);
+      if (jrefNode) {
+        jrefDocumentNode.children.push(jrefNode);
+      }
+
+      return jrefDocumentNode;
     } catch (error) {
       if (error instanceof VFileMessage) {
         return file.fail(error.message, /** @type Options */ (error));
--- src/json/rejson-parse.js
+++ src/json/rejson-parse.js
@@ -5,19 +5,34 @@ import { fromJson } from "./jsonast-util.js";
  * @import { Plugin } from "unified"
  * @import { VFile } from "vfile"
  * @import { Options } from "vfile-message"
- * @import { JsonDocumentNode } from "./jsonast.js"
+ * @import { JsonDocumentNode, JsonNode } from "./jsonast.js"
+ * @import { Reviver } from "./jsonast-util.js"
  */
 
 
-/** @type Plugin<[], string, JsonDocumentNode> */
-export function rejsonParse() {
+/**
+ * @typedef {{
+ *   reviver?: Reviver<JsonNode | undefined>;
+ * }} JsonParseOptions
+ */
+
+/** @type Plugin<[JsonParseOptions?], string, JsonDocumentNode> */
+export function rejsonParse(options) {
   /** @type (document: string, file: VFile) => JsonDocumentNode */
   this.parser = function (document, file) {
     try {
-      return {
+      /** @type JsonDocumentNode */
+      const jsonDocumentNode = {
         type: "json-document",
-        children: [fromJson(document)]
+        children: []
       };
+
+      const jsonNode = fromJson(document, options?.reviver);
+      if (jsonNode) {
+        jsonDocumentNode.children.push(jsonNode);
+      }
+
+      return jsonDocumentNode;
     } catch (error) {
       if (error instanceof VFileMessage) {
         return file.fail(error.message, /** @type Options */ (error));
