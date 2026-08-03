1c350d5eb8ba8a58045660479729cab8b86ce094
Jason Desrosiers <jdesrosi@gmail.com>
2026-08-03 11:54:45 -0700

    Expose getFileMediaType

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -42,6 +42,7 @@ export const addMediaTypePlugin: (contentType: string, plugin: MediaTypePlugin) 
 export const removeMediaTypePlugin: (contentType: string) => void;
 export const setMediaTypeQuality: (contentType: string, quality: number) => void;
 export const acceptableMediaTypes: () => string;
+export const getFileMediaType: (path: string) => Promise<string>;
 
 export class UnsupportedMediaTypeError extends Error {
   public constructor(mediaType: string, message?: string);
